570816980737ffe34306314b415707ed219eac63
Estheradorable <113927032+Estheradorable@users.noreply.github.com>
2024-04-21 16:16:28 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,93 @@
+ /*console.log("Hello world");
+console.log("My name is Esther");*/
+var myDog = "Jack";
+/*let myDog = "Bingo";*/
+const ourDog = "Bella"
+/*console.log(myDog);
+console.log(ourDog);*/
+
+/*const ourDog = "Robin";
+console.log(ourDog);*/
+
+let bootCamp = "JavaScript class";
+/*variable naming
+ camelCase, pascalCase
+myChildrenTeacher
+bread12_3
+case sensitive
+message and
+let myFriend = "Prisca";
+console.log(MyFriend);*/
+//console.log(bootCamp.length);
+//console.log(bootCamp[0])
+
+
+//Manipulation of variables
+//let  number = 9;
+//console.log(number)
+//number = number - 4;
+//console.log(number)
+//number+=10;
+//number--;
+//console.log(number)
+
+
+//functions
+/*function message ()
+{
+  console.log("Hello Prisca")
+}
+
+message()
+
+const message1= () => "Hello Esther";
+
+console.log(message1())
+
+//function parameter
+function average (num1, num2){
+  return(num1+num2)/2;
+}
+
+console.log(average(12,14))
+function sayHello(name){
+  return "Hello" + " " + name;  
+"H
+
+}
+
+console.log(sayHello("Esther"))
+
+        
+write a function that multiplies two numbers
+write a finction that will return the factorial of*/
+
+
+
+//;console.log(average(10, 16))
+//console.log(ave
+
+
+const popLang = ["Igbo", "Yoruba", "Hausa"]
+
+console.log(popLang)
+const firstLang = popLang[0]
+
+const numberOfLang = popLang.length;
+
+console.log(numberOfLang)
+
+//methods of Array
+//.push();
+//.pop();
+//.unshift();
+//.splice()
+
+popLang.push("Efik");
+console.log(popLang);
+
+const food = ["rice", "beams", "yam", "meat", "eggs"];
+
+food.pop();
+console.log(food)
+
